fcd5516a5e041629e5f59f9d686cc413183e42db
**Anmol** <**ass53**@sfu.ca>
2024-03-05 05:11:46 -0800

    Added a test note and used css to create a UI for the initial components

--- notes-app/src/App.tsx
+++ notes-app/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useState} from "react";
 import logo from './logo.svg';
 import './App.css';
 
@@ -10,8 +11,18 @@ const App = () => {
         <textarea placeholder = "Note" rows = {10} required/>
         <button type = "submit">Create Note</button>
       </form>
+      <div className="NoteGrid">
+        <div className="NoteItem">
+          <div className="NoteHeader">
+            <button>x</button>
+          </div>
+          <h2>Title</h2>
+          <p>content</p>
+        </div>
+      </div>
     </div>
   );
 };
+  
 
 export default App;
